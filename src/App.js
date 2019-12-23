db7831c4e141c7437e9e129e4f531f5f0f2d5f1c
Scott McAllister <stmcallister@gmail.com>
2019-12-23 00:23:54 -0800

    wip. pushing to change machines

--- src/App.js
+++ src/App.js
@@ -3,24 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let people = ["Scott","Amber","Ashley","Andrew","Katie","Emily","Jen"];
+  let receivers = ["Amber","Ashley","Andrew","Katie","Scott","Emily","Jen"];
+  let matches = {};
+  const prevMatches = {
+    "Amber": "Emily",
+    "Scott": "Amber",
+    "Ashley": "Scott",
+    "Andrew": "",
+    "Katie": "",
+    "Emily": ""
+  };
+
+  people.map(person => {
+      let pool = receivers.filter(name => name != person);
+      matches[person] = pool[Math.floor(Math.random() * pool.length)];
+      receivers = receivers.filter(n => n != matches[person]);
+  });
+  let matchesAry = Object.entries(matches);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Secret Santa App!
+        <ul>
+          {
+            matchesAry.map(match => {
+              return <div style={{margin:15}} onClick={() => alert(`Hello, ${match[0]}!  Your secret sibling to serve this week is...${match[1]}!`)}>{match[0]}</div>
+            })
+          }
+        </ul>
       </header>
     </div>
   );
 }
+
 
 export default App;
