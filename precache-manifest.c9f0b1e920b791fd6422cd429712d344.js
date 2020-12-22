fa5b4592b65c687e2be6bed012ebcd0a7ad8a3fd
Scott McAllister <smcallister@pagerduty.com>
2020-12-22 10:39:57 -0800

    Updates

---
+++ precache-manifest.c9f0b1e920b791fd6422cd429712d344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5c1cb537f39265dab2d466c4ed0b81",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "77e44ee3909b74ba4f8d",
+    "url": "/secret-santa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f1de15e3565acb1efb34",
+    "url": "/secret-santa/static/js/2.8d658bcf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/secret-santa/static/js/2.8d658bcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77e44ee3909b74ba4f8d",
+    "url": "/secret-santa/static/js/main.9061499c.chunk.js"
+  },
+  {
+    "revision": "4d5b6d00017eff157e06",
+    "url": "/secret-santa/static/js/runtime-main.d82b7abf.js"
+  }
+]);
