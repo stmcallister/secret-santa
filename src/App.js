afe4c30b4d03a95daf4fa0050b04402748ed1fc9
Scott McAllister <smcallister@pagerduty.com>
2020-12-22 10:21:09 -0800

    moving to localStorage

--- src/App.js
+++ src/App.js
@@ -2,48 +2,98 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  let givers = ["Scott","Amber","Ashley","Andrew","Katie","Emily","Jen"];
-  let receivers = ["Scott","Amber","Ashley","Andrew","Katie","Emily","Jen"];
+  let givers = ["Dad","Mom","Ashley","Andrew","Katie","Emily","Jen"];
+  let receivers = ["Dad","Mom","Ashley","Andrew","Katie","Emily","Jen"];
   let matches = {};
-  const prevMatches = {
-    "Amber": "Emily",
-    "Scott": "Amber",
-    "Ashley": "Scott",
-    "Andrew": "Ashley",
-    "Katie": "Andrew",
-    "Emily": "Katie"
+  matches = {
+    "12-6-2020": {
+      "Dad": "Katie",
+      "Mom": "Emily",
+      "Ashley": "Mom",
+      "Andrew": "Dad",
+      "Katie": "Ashley",
+      "Emily": "Andrew"
+    },
+    "12-13-2020": {
+      "Dad": "Andrew",
+      "Mom": "Ashley",
+      "Ashley": "Dad",
+      "Andrew": "Emily",
+      "Katie": "Mom",
+      "Emily": "Katie"
+    },
   };
+
   const santaStorageKey = 'secret-santa-matches';
   let matchesAry = [];
+
+  // if storage item doesn't exist, add defined matches
+  localStorage.setItem(santaStorageKey, JSON.stringify(matches));  
+
+
   createMatches();
 
-  
+  matchesAry = getCurrentMatchesArray(getCurrentMatches());
+
+  function getPrevReceivers(name, matches) {
+    let prevRec = [];
+    for (const [key, value] of Object.entries(matches)) {
+      prevRec.push(value[name]);
+    }
+ 
+    return prevRec;
+  }
+
   function createMatches() {
     console.log('createMatches');
-    matches = {};
-    if (!sessionStorage.getItem(santaStorageKey)) {
-      givers.map(person => {
-        let pool = receivers.filter(name => name !== person);
-        let randIndex = Math.floor(Math.random() * pool.length);
-        while(pool[randIndex] === prevMatches[person]) {
-          randIndex = Math.floor(Math.random() * pool.length);
-        }
-        matches[person] = pool[randIndex];
-        receivers = receivers.filter(n => n !== matches[person]);
-      });
-      sessionStorage.setItem(santaStorageKey, JSON.stringify(matches));  
-    } else {
-      matches = JSON.parse(sessionStorage.getItem(santaStorageKey));
-    }
-    matchesAry = Object.entries(matches);
+    let todaysMatches = {};
+    const dateStr = new Date().toLocaleDateString().replaceAll(/\//g, '-');
+
+    // get matches from localstorage
+    let storedMatches = JSON.parse(localStorage.getItem(santaStorageKey));
+
+    givers.forEach(person => {
+      // get available receivers by filtering out giver from list of possible receivers
+      let pool = receivers.filter(name => name !== person);
+
+      // get all previous receivers for the giver person
+      const prevReceivers = getPrevReceivers(person, storedMatches);
+
+      // randomly pick a number between 0 and length of receivers
+      let randIndex = Math.floor(Math.random() * pool.length);
+      
+      // while the randomly selected receiver matches 
+      while(prevReceivers.includes(pool[randIndex])) {
+        randIndex = Math.floor(Math.random() * pool.length);
+      }
+      // match found! set it to the matches object
+      todaysMatches[person] = pool[randIndex];
+
+      // review the matched receiver from the available receivers list
+      receivers = receivers.filter(n => n !== todaysMatches[person]);
+    });
+
+    // add todays matches to matches
+    storedMatches[dateStr] = todaysMatches;
+    localStorage.setItem(santaStorageKey, JSON.stringify(storedMatches));  
+  }
+
+  function getCurrentMatches() {
+    const stored = JSON.parse(localStorage.getItem(santaStorageKey));
+    // get the last element in the stored matches object and return it
+    return stored[Object.keys(stored)[Object.keys(stored).length - 1]];
+  }
+
+  function getCurrentMatchesArray(currentObj) {
+    return Object.entries(currentObj);
   }
 
   function resetMatches() {
-    sessionStorage.removeItem(santaStorageKey); 
-    // createMatches(); 
+    localStorage.removeItem(santaStorageKey); 
+    createMatches(); 
   }
   return (
-    <div className="App">
+<div className="App">
       <header className="App-header">
         <h1>Secret Santa App!</h1>
         <p style={{fontStyle: "italic"}}>Click on your name to see who you are serving this week!</p>
@@ -52,11 +102,10 @@ function App() {
               return <div style={{margin:15}} onClick={() => alert(`Hello, ${match[0]}!  Your secret sibling to serve this week is...${match[1]}!`)}>{match[0]}</div>
             })
           }
-        <div><button onClick={() => resetMatches() }>Clear Matches!</button></div>
+        <div><button onClick={() => resetMatches() }>Get New Matches!</button></div>
       </header>
     </div>
   );
 }
-
 
 export default App;
